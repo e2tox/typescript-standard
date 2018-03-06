62a0c80af064da2ba1015cfcbbb95d00b42a724d
e2tox <e2tox@live.com>
2018-03-06 17:53:16 +0800

    test formatter

--- src/lib/formatter/formatter.ts
+++ src/lib/formatter/formatter.ts
@@ -2,6 +2,22 @@ import * as ts from 'typescript'
 import * as path from 'path'
 import { loadText, saveText } from '../utils/index'
 
+export class LanguageServiceHost implements ts.LanguageServiceHost {
+  files: ts.MapLike<ts.IScriptSnapshot> = {};
+
+  addFile(fileName: string, text: string) {
+    this.files[fileName] = ts.ScriptSnapshot.fromString(text);
+  }
+
+  // for ts.LanguageServiceHost
+  getCompilationSettings = () => ts.getDefaultCompilerOptions();
+  getScriptFileNames = () => Object.keys(this.files);
+  getScriptVersion = (_fileName: string) => '0';
+  getScriptSnapshot = (fileName: string) => this.files[fileName];
+  getCurrentDirectory = () => process.cwd();
+  getDefaultLibFileName = (options: ts.CompilerOptions) => ts.getDefaultLibFilePath(options);
+}
+
 export class Formatter {
 
   option: ts.FormatCodeOptions;
@@ -9,8 +25,11 @@ export class Formatter {
 
   constructor(option: ts.FormatCodeOptions) {
     this.option = option;
-    this.ruleProvider = new (ts as any).formatting.RulesProvider();
-    this.ruleProvider.ensureUpToDate(this.option);
+    // ts 2.2
+    if ((ts as any).formatting.RulesProvider) {
+      this.ruleProvider = new (ts as any).formatting.RulesProvider();
+      this.ruleProvider.ensureUpToDate(this.option);
+    }
   }
 
   static applyEdits(text: string, edits: ts.TextChange[]): string {
@@ -25,14 +44,61 @@ export class Formatter {
     return result;
   }
 
-  // https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#pretty-printer-using-the-ls-formatter
   format(file: string): boolean {
+    if (this.ruleProvider) {
+      return this.format22(file);
+    }
+    else {
+      return this.format27(file);
+    }
+  }
+
+  // ts 2.7
+  format27(file: string): boolean {
+
+    const filename = path.basename(file);
+    let fileContent = loadText(file);
+
+    const host = new LanguageServiceHost();
+    host.addFile(filename, fileContent);
+
+    const languageService = ts.createLanguageService(host);
+    const edits = languageService.getFormattingEditsForDocument(filename, this.option);
+    edits
+      .sort((a, b) => a.span.start - b.span.start)
+      .reverse()
+      .forEach(edit => {
+        const head = fileContent.slice(0, edit.span.start);
+        const tail = fileContent.slice(edit.span.start + edit.span.length);
+        fileContent = `${head}${edit.newText}${tail}`;
+      });
+
+    if (edits.length) {
+      saveText(file, fileContent);
+      return true;
+    }
+    else {
+      return false;
+    }
+
+  }
+
+  // ts 2.2
+  // https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#pretty-printer-using-the-ls-formatter
+  format22(file: string): boolean {
     const filename = path.basename(file);
     const fileContent = loadText(file);
     const sourceFile = ts.createSourceFile(filename, fileContent, ts.ScriptTarget.Latest, true);
 
     // Get the formatting edits on the input sources
-    const edits = (ts as any).formatting.formatDocument(sourceFile, this.ruleProvider, this.option);
+    let edits;
+
+    if (this.ruleProvider) {
+      edits = (ts as any).formatting.formatDocument(sourceFile, this.ruleProvider, this.option);
+    }
+    else {
+      edits = (ts as any).formatting.formatDocument(sourceFile, this.option);
+    }
 
     if (edits.length) {
       // Apply the edits on the input code
@@ -44,6 +110,6 @@ export class Formatter {
       return false;
     }
 
-  }
+}
 
 }
